dccc84a0ff8c138f142af530a23b20006b478b07
Eslaro <eslaro47@gmail.com>
2025-12-06 08:08:14 -0500

    Coomit!

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,11 +1,14 @@
-## Wordcount
-
 # Ejemplo para casos de uso
 # python3 -m homework data/input data/output
 
 
 import argparse
-import sys
+
+from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
 
 def parse_args():
@@ -25,9 +28,9 @@ def parse_args():
 
 
 def main():
-    input, output = parse_args()
-    print(f"input: {input}")
-    print(f"output: {output}")
-
-
-# Función test_read_all_lines
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
